925b1105d066c74742f659181f1ef222bedc7082
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 21:01:12 +0800

    chore: update import

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { config } from 'dotenv'
 import { Hono } from 'hono'
 
-import pkg from '../package.json' with { type: 'json' }
+import { version } from '../package.json' with { type: 'json' }
 import { translate } from './api/translate'
 
 config()
 
 export const app = new Hono()
-  .get('/', c => c.text(`ARPK v${pkg.version}`))
+  .get('/', c => c.text(`ARPK v${version}`))
   .route('/translate', translate)
   .route('/api/v1/translate', translate)
   .route('/api/v2/translate', translate)
