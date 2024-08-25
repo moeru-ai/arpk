9403736a091a538992eb84a85abf3e0a9da32be1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 23:45:25 +0800

    refactor(translate): improve prompt

--- src/api/translate.ts
+++ src/api/translate.ts
@@ -21,30 +21,24 @@ export const translate = new Hono()
   .post('/', async (c) => {
     const { source_lang, target_lang, text } = await c.req.json<Options>()
 
-    const response = await ollama.chat({
-      messages: [
-        {
-          content: [
-            'You are a professional translator.',
-            `Please translate the following into ${languages
-              .find(({ language }) => language === target_lang)
-              ?.name ?? target_lang
-            }, do not give any text other than the translated content:`,
-          ].join(' '),
-          role: 'system',
-        },
-        {
-          content: text,
-          role: 'user',
-        },
-      ],
+    const sourceLangName = languages.find(({ language }) => language === source_lang)?.name ?? source_lang
+    const targetLangName = languages.find(({ language }) => language === target_lang)?.name ?? target_lang
+
+    const { response } = await ollama.generate({
       model: env.ARPK_MODEL ?? 'llama3.1',
+      prompt: text,
+      system: [
+        'You are a professional translator.',
+        `Please translate the following in ${sourceLangName} into ${targetLangName},`,
+        'do not give any text or line breaks other than the translated content,',
+        'and do not insert line breaks at the end:',
+      ].join(' '),
     })
 
     return c.json({
       alternates: [],
       code: 200,
-      data: response.message.content,
+      data: response,
       id: Date.now(),
       method: 'ARPK',
       source_lang,
