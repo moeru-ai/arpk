f307b95de729e5201f8edbb5826c94bcd7ac5adb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-30 12:16:27 +0800

    refactor(prompts): ignore auto source lang

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -7,14 +7,16 @@ import { getLanguageName } from './utils'
 const source_lang = '{{source_lang}}'
 const target_lang = '{{target_lang}}'
 
-const defaultSystemPrompt = [
+const defaultSystemPrompt = (options: Omit<Options, 'text'>) => [
   'You are a professional translator.',
-  `please translate the following in ${source_lang} into ${target_lang},`,
+  options.source_lang.toLowerCase() === 'auto'
+    ? `please translate the following into ${target_lang},`
+    : `please translate the following in ${source_lang} into ${target_lang},`,
   'do not give any text other than the translated content,',
   'and trim the spaces at the end:',
 ].join(' ')
 
 export const systemPrompt = (options: Omit<Options, 'text'>) =>
-  (env.ARPK_SYSTEM_PROMPT ?? defaultSystemPrompt)
+  (env.ARPK_SYSTEM_PROMPT ?? defaultSystemPrompt(options))
     .replace(source_lang, getLanguageName(options.source_lang))
     .replace(target_lang, getLanguageName(options.target_lang))
