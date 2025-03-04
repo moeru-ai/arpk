c202bd0cb9f507c9b5ee42fae3fbcd64b0b07e58
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-04 16:36:22 +0800

    fix: lint errors

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,11 @@
+import { GLOB_MARKDOWN_CODE } from '@antfu/eslint-config'
 import { defineConfig } from '@importantimport/eslint-config'
 
-export default defineConfig({ typescript: { tsconfigPath: './tsconfig.json' } })
+export default defineConfig({ typescript: { tsconfigPath: './tsconfig.json' } }, {
+  files: [GLOB_MARKDOWN_CODE],
+  rules: {
+    '@masknet/no-then': 'off',
+    '@masknet/no-top-level': 'off',
+    '@masknet/unicode-specific-set': 'off',
+  },
+})
