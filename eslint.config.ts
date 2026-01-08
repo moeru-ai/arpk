22f6d3eef2987ebff9cd1fadfd4dc4d0ab544215
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-08 15:28:42 +0800

    chore: use moeru config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,1 @@
-import { GLOB_MARKDOWN_CODE } from '@antfu/eslint-config'
-import { defineConfig } from '@importantimport/eslint-config'
-
-export default defineConfig({ typescript: { tsconfigPath: './tsconfig.json' } }, {
-  files: [GLOB_MARKDOWN_CODE],
-  rules: {
-    '@masknet/no-then': 'off',
-    '@masknet/no-top-level': 'off',
-    '@masknet/unicode-specific-set': 'off',
-  },
-})
+export { default } from '@moeru/eslint-config'
